7062cdf3eecf3d7e14223790534967dc81958bbd
Don <don@greenbilling.com>
2026-05-29 06:17:47 +1000

    Add routine start behaviour

--- src/routine_engine.py
+++ src/routine_engine.py
@@ -1,2 +1,12 @@
 def list_routines(routine_store):
     return routine_store
+
+
+def start_routine(routine_store, routine_id):
+    routine = routine_store[routine_id]
+
+    return {
+        "routine_id": routine_id,
+        "routine_name": routine["name"],
+        "status": "active",
+    }
--- tests/test_routine_engine.py
+++ tests/test_routine_engine.py
@@ -1,4 +1,4 @@
-from src.routine_engine import list_routines
+from src.routine_engine import list_routines, start_routine
 
 
 def test_list_routines_returns_supplied_routines():
@@ -11,3 +11,17 @@ def test_list_routines_returns_supplied_routines():
     routines = list_routines(routine_store)
 
     assert routines == routine_store
+
+
+def test_start_routine_creates_active_routine_state():
+    routine_store = {
+        "example-routine": {
+            "name": "Example Routine",
+        }
+    }
+
+    active_routine = start_routine(routine_store, "example-routine")
+
+    assert active_routine["routine_id"] == "example-routine"
+    assert active_routine["routine_name"] == "Example Routine"
+    assert active_routine["status"] == "active"
